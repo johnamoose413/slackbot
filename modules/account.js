d8eef59dc32bd9e8fd3fc032c5aed0bb9b94e61a
John McGowan <jmcgowan@ferrarienergy.com>
2017-09-05 16:50:39 -0600

    no message

--- modules/account.js
+++ modules/account.js
@@ -14,25 +14,26 @@ exports.execute = (req, res) => {
 
     let slackUserId = req.body.user_id,
         oauthObj = auth.getOAuthObject(slackUserId),
-        q = "SELECT Id, Name, NMA__c, Dollar_NMA__c FROM Land_Holding__c WHERE Account__r.Name LIKE '%" + req.body.text + "%'";
+        q = "SELECT Id, Name, Phone FROM Account WHERE Name LIKE '%" + req.body.text + "%' LIMIT 1",
+        lhQ = "SELECT Id, Name, NMA__c, Dollar_NMA__c FROM Land_Holding__c WHERE Account__c = :" + account[0].Id;
+
+    var account;
 
     force.query(oauthObj, q)
-        .then(data => {
-            let landHoldings = JSON.parse(data).records;
+        .then(date => {
+            account = JSON.parse(data).records;
 
-            if (landHoldings && landHoldings.length>0) {
+            if(account && account.length > 0) {
                 let attachments = [];
-                landHoldings.forEach(function(landHolding) {
-                    let fields = [];
-                    fields.push({title: "Name", value: landHolding.Name, short:true});
-                    fields.push({title: "NMA", value: landHolding.NMA__c, short:true});
-                    fields.push({title: "Dollar/NMA", value: landHolding.Dollar_NMA__c, short:true});
-                    fields.push({title: "Open in Salesforce:", value: oauthObj.instance_url + "/" + landHolding.Id, short:false});
-                    attachments.push({color: "#6b3021", fields: fields});
-                });
-                res.json({text: "Land Holdings Belonging To '" + req.body.text + "':", attachments: attachments});
+                let fields = [];
+
+                fields.push({title: "Name", value: account[0].Name, short: true});
+                fields.push({title: "Phone", value: account[0].Phone, short: true});
+                fields.push({title: "Open in Salesforce:", value: oauthObj.instance_url + "/" + account[0].Id, short: false});
+                attachments.push({color: "#236fbd", fields: fields});
+                res.json({text: "Account Name: " + account[0].Name + " ", attachments: attachments});
             } else {
-                res.send("No records");
+                res.send("No Account records");
             }
         }).catch(error => {
             if (error.code == 401) {
@@ -41,4 +42,32 @@ exports.execute = (req, res) => {
                 res.send("An error as occurred " + error);
             }
         });
+
+    if(account) {
+        force.query(oauthObj, lhQ)
+            .then(data => {
+                let landHoldings = JSON.parse(data).records;
+
+                if (landHoldings && landHoldings.length>0) {
+                    let attachments = [];
+                    landHoldings.forEach(function(landHolding) {
+                        let fields = [];
+                        fields.push({title: "Name", value: landHolding.Name, short:true});
+                        fields.push({title: "NMA", value: landHolding.NMA__c, short:true});
+                        fields.push({title: "Dollar/NMA", value: landHolding.Dollar_NMA__c, short:true});
+                        fields.push({title: "Open in Salesforce:", value: oauthObj.instance_url + "/" + landHolding.Id, short:false});
+                        attachments.push({color: "#6b3021", fields: fields});
+                    });
+                    res.json({text: "Land Holdings Belonging To '" + req.body.text + "':", attachments: attachments});
+                } else {
+                    res.send("No Land Holding records");
+                }
+            }).catch(error => {
+                if (error.code == 401) {
+                    res.send(`Visit this URL to login to Salesforce: https://${req.hostname}/login/` + slackUserId);
+                } else {
+                    res.send("An error as occurred " + error);
+                }
+            });
+    }
 };
