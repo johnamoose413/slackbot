33cc5da526b4f7c211777e462eda3da210b39087
John McGowan <jmcgowan@ferrarienergy.com>
2017-09-05 17:30:36 -0600

    no message

--- modules/account.js
+++ modules/account.js
@@ -30,7 +30,6 @@ exports.execute = (req, res) => {
                 fields.push({title: "Phone", value: account[0].Phone, short: true});
                 fields.push({title: "Open in Salesforce:", value: oauthObj.instance_url + "/" + account[0].Id, short: false});
                 attachments.push({color: "#236fbd", fields: fields});
-                //res.json({text: "Account Name: " + account[0].Name + " ", attachments: attachments});
 
                 let lhQ = "SELECT Id, Name, NMA__c, Dollar_NMA__c FROM Land_Holding__c WHERE Account__c = '" + account[0].Id + "'";
 
@@ -39,16 +38,15 @@ exports.execute = (req, res) => {
                             let landHoldings = JSON.parse(data).records;
 
                             if (landHoldings && landHoldings.length>0) {
-                                let atts = [];
                                 landHoldings.forEach(function(landHolding) {
                                     let flds = [];
                                     flds.push({title: "Name", value: landHolding.Name, short:true});
                                     flds.push({title: "NMA", value: landHolding.NMA__c, short:true});
                                     flds.push({title: "Dollar/NMA", value: landHolding.Dollar_NMA__c, short:true});
                                     flds.push({title: "Open in Salesforce:", value: oauthObj.instance_url + "/" + landHolding.Id, short:false});
-                                    atts.push({color: "#6b3021", fields: flds});
+                                    attachments.push({color: "#6b3021", fields: flds});
                                 });
-                                res.json({text: "Land Holdings Belonging To '" + req.body.text + "':", attachments: atts});
+                                res.json({text: "Land Holdings Belonging To '" + account[0].Name + "':", attachments: atts});
                             } else {
                                 res.send("No Land Holding records");
                             }
